6daa79e17a73b4f55f0ae2c73901cd48d267871a
AlgotOlofsson <algot@omail.se>
2025-06-05 20:53:55 +0200

    Sökfunktion
    Jag fixade en sökfuntkion genom filteredSports, searchTerm state och la till inputfält från min branch.

--- src/SearchApp.js
+++ src/SearchApp.js
@@ -9,6 +9,7 @@ const API_URL = `https://api.the-odds-api.com/v4/sports/?apiKey=${API_KEY}`;
 const SearchApp = () => {
   const [sports, setSports] = useState([]);
   const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   // Favoriter sparas i localStorage
   const [favorites, setFavorites] = useState(
@@ -32,19 +33,32 @@ const SearchApp = () => {
     localStorage.setItem("favorites", JSON.stringify(favorites));
   }, [favorites]);
 
+  const filteredSports = sports.filter((sport) =>
+    sport.group.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    sport.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="container mt-4">
       <h2 className="mb-3">Sports Odds Finder</h2>
+      <div className="d-flex justify-content-end mb-3 position-relative">
+        <div className="w-50">
+            <input className="form-control form-control-sm w-100 mb-3 ms-auto" type="text" placeholder="Sök sport eller liga..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+            {searchTerm.trim() !== "" && (
+                <div className="bg-white border rounded shadow-sm mt-1 position-absolute w-50" style={{ textAlign: "center", zIndex: 1000, maxHeight: "300px", overflowY: "auto" }}>
+                    <SearchResults
+                    sports={filteredSports}
+                    favorites={favorites}
+                    setFavorites={setFavorites}
+                    />
+                </div>
+            )}
+        </div>
+      </div>  
       <FavoritesCalendar favorites={favorites} />
       {error ? (
         <p className="text-danger">{error}</p>
-      ) : (
-        <SearchResults
-          sports={sports}
-          favorites={favorites}
-          setFavorites={setFavorites}
-        />
-      )}
+      ): null}
     </div>
   );
 };
